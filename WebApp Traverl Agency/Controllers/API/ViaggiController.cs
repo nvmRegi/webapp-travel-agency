cc9ff801f486ed49f906b2ce180b7285b773c10b
regi <cabael.regine@gmail.com>
2022-05-23 14:33:55 +0200

    HomePage

---
+++ WebApp Traverl Agency/Controllers/API/ViaggiController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApp_Traverl_Agency.Data;
+using WebApp_Traverl_Agency.Models;
+
+namespace WebApp_Traverl_Agency.Controllers.API
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class ViaggiController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            List<PacchettoViaggio> listaViaggi = new List<PacchettoViaggio>();
+
+            using(TravelContext db = new TravelContext())
+            {
+                listaViaggi = db.Pacchetto_Viaggio.ToList();
+            }
+
+            return Ok(listaViaggi);
+        }
+    }
+}
